970e53f47db61ef0d1c3d315c10edf46c5e6bd75
Miguel Ticaray <142277133+metfdev@users.noreply.github.com>
2025-12-08 08:07:21 -0400

    update db

--- DB/ccrs.sql
+++ DB/ccrs.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-12-2025 a las 21:49:56
+-- Tiempo de generación: 08-12-2025 a las 13:06:51
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,16 +36,19 @@ CREATE TABLE `cotizaciones` (
   `año_carro` text NOT NULL,
   `placa_carro` text NOT NULL,
   `vin_carro` text NOT NULL,
-  `data_repuestos` text CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL
+  `data_repuestos` text NOT NULL,
+  `fecha` date NOT NULL,
+  `estado` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `cotizaciones`
 --
 
-INSERT INTO `cotizaciones` (`id`, `id_users`, `departamento`, `nombre_cliente`, `modelo_carro`, `año_carro`, `placa_carro`, `vin_carro`, `data_repuestos`) VALUES
-(1, 3, 'Repuestos', 'Cliente prueba', 'Hilux', '2005', 'AD052CD', '11111111111111111', '{\"repuestos\":[\r\n{\r\n\"id\": 1,\r\n\"nro_parte\":00000888882222,\r\n\"nombre\": \"repuesto prueba1\",\r\n\"cantidad\": \"2\"\r\n},\r\n{\r\n\"id\": 2,\r\n\"nro_parte\":00000888882223,\r\n\"nombre\": \"repuesto prueba2\",\r\n\"cantidad\": \"3\"\r\n},\r\n{\r\n\"id\": 3,\r\n\"nro_parte\":00000888882224,\r\n\"nombre\": \"repuesto prueba3\",\r\n\"cantidad\": \"1\"\r\n},\r\n{\r\n\"id\": 4,\r\n\"nro_parte\":00000888882225,\r\n\"nombre\": \"repuesto prueba4\",\r\n\"cantidad\": \"4\"\r\n}\r\n]\r\n}'),
-(2, 2, 'Repuestos', 'Cliente prueba', 'Hilux', '2025', 'AD052BN', '11111111111111111', '{\"repuestos\":[\r\n{\r\n\"id\": 1,\r\n\"nro_parte\":00000888882222,\r\n\"nombre\": \"repuesto prueba1\",\r\n\"cantidad\": \"2\"\r\n},\r\n{\r\n\"id\": 2,\r\n\"nro_parte\":00000888882223,\r\n\"nombre\": \"repuesto prueba2\",\r\n\"cantidad\": \"3\"\r\n},\r\n{\r\n\"id\": 3,\r\n\"nro_parte\":00000888882224,\r\n\"nombre\": \"repuesto prueba3\",\r\n\"cantidad\": \"1\"\r\n},\r\n{\r\n\"id\": 4,\r\n\"nro_parte\":00000888882225,\r\n\"nombre\": \"repuesto prueba4\",\r\n\"cantidad\": \"4\"\r\n}\r\n]\r\n}');
+INSERT INTO `cotizaciones` (`id`, `id_users`, `departamento`, `nombre_cliente`, `modelo_carro`, `año_carro`, `placa_carro`, `vin_carro`, `data_repuestos`, `fecha`, `estado`) VALUES
+(1, 3, 'Repuestos', 'Cliente prueba', 'Hilux', '2005', 'AD052CD', '11111111111111111', '{\"repuestos\":[\r\n{\r\n\"id\": 1,\r\n\"nro_parte\":00000888882222,\r\n\"nombre\": \"repuesto prueba1\",\r\n\"cantidad\": \"2\"\r\n},\r\n{\r\n\"id\": 2,\r\n\"nro_parte\":00000888882223,\r\n\"nombre\": \"repuesto prueba2\",\r\n\"cantidad\": \"3\"\r\n},\r\n{\r\n\"id\": 3,\r\n\"nro_parte\":00000888882224,\r\n\"nombre\": \"repuesto prueba3\",\r\n\"cantidad\": \"1\"\r\n},\r\n{\r\n\"id\": 4,\r\n\"nro_parte\":00000888882225,\r\n\"nombre\": \"repuesto prueba4\",\r\n\"cantidad\": \"4\"\r\n}\r\n]\r\n}', '2025-12-01', 'aprobado'),
+(2, 2, 'Repuestos', 'Cliente prueba', 'Hilux', '2025', 'AD052BN', '11111111111111111', '{\"repuestos\":[\r\n{\r\n\"id\": 1,\r\n\"nro_parte\":00000888882222,\r\n\"nombre\": \"repuesto prueba1\",\r\n\"cantidad\": \"2\"\r\n},\r\n{\r\n\"id\": 2,\r\n\"nro_parte\":00000888882223,\r\n\"nombre\": \"repuesto prueba2\",\r\n\"cantidad\": \"3\"\r\n},\r\n{\r\n\"id\": 3,\r\n\"nro_parte\":00000888882224,\r\n\"nombre\": \"repuesto prueba3\",\r\n\"cantidad\": \"1\"\r\n},\r\n{\r\n\"id\": 4,\r\n\"nro_parte\":00000888882225,\r\n\"nombre\": \"repuesto prueba4\",\r\n\"cantidad\": \"4\"\r\n}\r\n]\r\n}', '2025-10-14', 'pendiente'),
+(3, 2, 'Repuestos', 'Cliente prueba', 'Hilux', '2025', 'AD052BN', '11111111111111111', '{\"repuestos\":[\r\n{\r\n\"id\": 1,\r\n\"nro_parte\":00000888882222,\r\n\"nombre\": \"repuesto prueba1\",\r\n\"cantidad\": \"2\"\r\n},\r\n{\r\n\"id\": 2,\r\n\"nro_parte\":00000888882223,\r\n\"nombre\": \"repuesto prueba2\",\r\n\"cantidad\": \"3\"\r\n},\r\n{\r\n\"id\": 3,\r\n\"nro_parte\":00000888882224,\r\n\"nombre\": \"repuesto prueba3\",\r\n\"cantidad\": \"1\"\r\n},\r\n{\r\n\"id\": 4,\r\n\"nro_parte\":00000888882225,\r\n\"nombre\": \"repuesto prueba4\",\r\n\"cantidad\": \"4\"\r\n}\r\n]\r\n}', '2025-09-16', 'rechazado');
 
 -- --------------------------------------------------------
 
@@ -69,8 +72,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `last_name`, `email`, `pass`, `cargo`, `departamento`, `rol`) VALUES
-(2, 'Miguel', 'Ticaray', 'miguelticaray@gmail.com', '$2y$10$O4igYxv7X3a8BKpOyxFULucZCNmXhD4l9uT1i7gEaOxhqYFSMfO3O', 'Soporte Tecnico', 'Informatica', 'admin'),
-(3, 'Aurelis', 'Dommar', 'aurelisdc21@gmail.com', '$2y$10$iSRsmwjoBhib6buD8iLXBe.lb.LvLqdKY7G.XEDFSwlCF8Q2KkMJW', 'Soporte Tecnico', 'Desarrollo', 'admin');
+(2, 'Miguel', 'Ticaray', 'miguelticaray@gmail.com', '$2y$10$O4igYxv7X3a8BKpOyxFULucZCNmXhD4l9uT1i7gEaOxhqYFSMfO3O', 'Soporte Tecnico', 'Servicio', 'admin'),
+(3, 'Aurelis', 'Dommar', 'aurelisdc21@gmail.com', '$2y$10$iSRsmwjoBhib6buD8iLXBe.lb.LvLqdKY7G.XEDFSwlCF8Q2KkMJW', 'Soporte Tecnico', 'Servicio', 'admin');
 
 --
 -- Índices para tablas volcadas
@@ -97,7 +100,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `cotizaciones`
 --
 ALTER TABLE `cotizaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
